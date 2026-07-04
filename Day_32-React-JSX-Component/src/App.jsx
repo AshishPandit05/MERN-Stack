50378865bb26acd3776b9a3e3d2c1dfd551ce499
AshishPandit05 <mr.ashishpandit05@gmail.com>
2026-07-04 20:58:46 +0530

    Day 32 React component complete

---
+++ Day_32-React-JSX-Component/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+let App = () => {
+  // let ui = React.cloneElement("div", {}, [
+  //   React.createElement("h1", {}, "hello"),
+  //   React.createElement("h2", {}, "byy"),
+  //   React.createElement("h3", {}, "return"),
+  // ]);
+
+  return;
+  <div>
+    <h1>hello</h1>
+    <h2>byy</h2>
+    <h3>return</h3>;
+  </div>;
+};
+
+export default App;
