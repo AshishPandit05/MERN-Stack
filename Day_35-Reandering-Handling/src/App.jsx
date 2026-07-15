d374bf58e4a5eb6d10a4fa7653652767971cee2f
AshishPandit05 <mr.ashishpandit05@gmail.com>
2026-07-15 10:34:55 +0530

    Project setup done

---
+++ Day_35-Reandering-Handling/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Render from "./render";
+import Web from "./Web";
+import WebCopy from "./WebCopy";
+
+const App = () => {
+  return (
+    <div>
+      {/* <Render /> */}
+      {/* <Web /> */}
+      <WebCopy />
+    </div>
+  );
+};
+
+export default App;
