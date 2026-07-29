6a290aaa6a45404427cdf7aa0b4cf5bcf878e0c7
AshishPandit05 <mr.ashishpandit05@gmail.com>
2026-07-29 17:32:00 +0530

    Day 38 complete

--- Day_38-RHF-Validation-project/src/App.jsx
+++ Day_38-RHF-Validation-project/src/App.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "./Components/Navbar";
+import UserCard from "./Components/UserCard";
+import UserForm from "./Components/UserForm";
+import { use } from "react";
 
 const App = () => {
+  const [toggle, setToggle] = useState(false);
+  const [users, setUsers] = useState([]);
+  console.log(users);
+
   return (
     <div>
-      <h3>Hello React</h3>
+      <Navbar setToggle={setToggle} />
+      {toggle ? (
+        users.map((elem) => {
+          return <UserCard user={elem} />;
+        })
+      ) : (
+        <div className="flex justify-center items-center m-25">
+          <UserForm setUsers={setUsers} setToggle={setToggle} />
+        </div>
+      )}
+      ;
     </div>
   );
 };
