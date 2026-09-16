afa4a1e16457e602d0a48460a450901cdd7d1d64
AshishPandit05 <mr.ashishpandit05@gmail.com>
2026-09-16 17:04:51 +0530

    pages switch using toggle

--- Day_43-React-router/src/App.jsx
+++ Day_43-React-router/src/App.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 
 const App = () => {
+  const [toggle, setToggle] = useState("true");
   return (
-    <div>
-      <h1>React</h1>
-    </div>
-  )
-}
+    <div className="bg-black h-screen">
+      <nav className="bg-gray-600 flex justify-between p-5 text-white text-xl">
+        <h2>Logo</h2>
+        <div className="flex gap-8 items-center ">
+          <p onClick={() => setToggle("Home")} className="cursor-pointer">
+            Home
+          </p>
+          <p onClick={() => setToggle("About")} className="cursor-pointer">
+            About
+          </p>
+          <p onClick={() => setToggle("Contact")} className="cursor-pointer">
+            Contact
+          </p>
+        </div>
+        <div>Login</div>
+      </nav>
 
-export default App
+      <div>
+        {toggle === "Home" && <Home />}
+        {toggle === "About" && <About />}
+        {toggle === "Contact" && <Contact />}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ Day_43-React-router/src/Pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h2 className="text-white text-2xl">This is Contact page</h2>
+    </div>
+  );
+};
+
+export default Contact;
